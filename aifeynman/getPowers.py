74a7855d051875e3d1a9f76303667415119ffb23
zykhoo <46671677+zykhoo@users.noreply.github.com>
2023-09-06 19:15:32 +0800

    Update getPowers.py

--- aifeynman/getPowers.py
+++ aifeynman/getPowers.py
@@ -28,10 +28,10 @@ def getPowers(N,a):
     rand_drop_cols = np.arange(0,len(N[0]),1)
     rand_drop_rows = np.arange(0,len(N),1)
     rand_drop_rows = np.flip(rand_drop_rows)
-    rank = matrix_rank(N)
-    d_cols = list(combinations(rand_drop_cols,len(N[0])-rank))
+    rank = matrix_rank(N) #find rank of matrix
+    d_cols = list(combinations(rand_drop_cols,len(N[0])-rank)) #create combinations of columns that when deleted result in full rank matrix (note, combinations, NOT permutations, order is kept)
     d_rows = list(combinations(rand_drop_rows,len(N)-rank))
-    for i in d_cols:
+    for i in d_cols: #purpose of this nested loop is to find the full rank matrix representing linear equations
         M = N
         M = np.delete(M,i,1)
         M = np.transpose(M)
@@ -44,12 +44,12 @@ def getPowers(N,a):
                 indices_powers = i
                 break
 
-    b = np.delete(a,indices_sol)
-    params = np.linalg.solve(solved_M,b)
+    b = np.delete(a,indices_sol) #create another vector representing solutions of the linear equations
+    params = np.linalg.solve(solved_M,b) #solve simultaneous equation problem
 
     sol = []
-    for i in range(len(N[0])):
-        if i in indices_powers:
+    for i in range(len(N[0])): #allocate the solutions of simultaneous equation problem to a vector to return it
+        if i in indices_powers: # if the index was to be removed, put i
             sol = sol + [0]
         else:
             sol = sol + [params[0]]
